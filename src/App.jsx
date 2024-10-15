7885769056351ca721025a4ed81fb61cb863bbf0
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-10-14 20:01:35 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { PhotosPage } from "./BreweriesPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PhotosPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/BreweriesPage.jsx
@@ -0,0 +1,7 @@
+export function BreweriesPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
